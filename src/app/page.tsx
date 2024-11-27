ad12cc29ff0ae8bda54fc679f30f434f168aafa7
michael1233 <emmamichael201@gmail.com>
2024-11-27 23:10:01 +0100

    completed : hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Navbar from "@/sections/Navbar";
 import Hero from "@/sections/Hero";
 export default function Home() {
     return(
-        <div>
+        <>
             <Navbar />
-           
-        </div>
+             <Hero />
+        </>
     );
 }
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,24 +1,18 @@
 import Button from "@/components/Button";
-import designExampletImage from "@/assets/images/design-example-1.png";
-import designExzmple2Image from "@/assets/images/design-example-2.png";
+import designEx1 from "@/assets/images/design-example-1.png";
+import designEx2 from "@/assets/images/design-example-2.png";
 import Image from "next/image";
 import Pointer from "@/components/Pointer";
 
-export default function Hero() {
+function Hero() {
     return (
         <section className="py-24 overflow-x-clip">
             <div className="container relative">
                 <div className="absolute -left-32 top-16 hidden lg:block">
-                    <Image
-                        src={designExampletImage}
-                        alt="Desig example 1 image"
-                    />
+                    <Image src={designEx1} alt="designEx1" />
                 </div>
-                <div className="absolute -right-64 top-16 hidden lg:block">
-                    <Image
-                        src={designExzmple2Image}
-                        alt="Design example 2 image"
-                    />
+                <div className="absolute -right-64 -top-16 hidden lg:block">
+                    <Image src={designEx2} alt="designEx1" />
                 </div>
                 <div className="absolute left-56 top-96 hidden lg:block">
                     <Pointer name="Andrea" />
@@ -27,34 +21,38 @@ export default function Hero() {
                     <Pointer name="Bryan" color="red" />
                 </div>
                 <div className="flex justify-center">
-                <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
-                    Member 401 Programming Communities 
+                    <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold cursor-pointer">
+                        {" "}
+                        ✨ $7.5 seed round raised
+                    </div>
                 </div>
-                </div>
-                <h1 className="text-6xl md:text-7x1 lg:text-8xl font-medium text-center mt-6">
-                    Programming  Communities
+
+                <h1 className="text-6xl font-medium lg:text-8xl text-center mt-6 md:text-7xl">
+                    Impactful design, created effortlessly
                 </h1>
+                <p className="text-center text-xl text-white/50 mt-8 mx-auto max-w-2xl">
+                    Design tools shouldn't slow you down, Layers combines
+                    powerful features with an intuitive interface that keeps you
+                    in your creative flow
+                </p>
+                <form className="flex border border-white/15 mx-auto max-w-lg rounded-full p-2 mt-8">
+                    <input
+                        className="bg-transparent px-4 md:flex-1 "
+                        type="email"
+                        placeholder="Enter your email"
+                    />
+                    <Button
+                        variant="primary"
+                        className="whitespace-nowrap"
+                        type="submit"
+                        size="sm"
+                    >
+                        Sign Up
+                    </Button>
+                </form>
             </div>
-            <p className="text-center text-xl text-white/50 mt-8 max-w-2xl mx-auto">
-                Everyone Don&apos;t Miss Opportunity. First Learn Then Earn.
-                Web Designing, English Speaking Course, Web, TypeScript,
-                 Next js.
-            </p>
-            <form className="flex border border-white/15 rounded-full p-2 mt-8 max-w-lg mx-auto">
-                <input
-                    type="email "
-                    placeholder="Enter your email"
-                    className="bg-transparent px-4 md:flex-1"
-                />
-                <Button
-                    type="submit"
-                    variant="primary"
-                    className="whitespace-nowrap"
-                    size="sm"
-                >
-                    Sign Up
-                </Button>
-            </form>
         </section>
     );
 }
+
+export default Hero;
